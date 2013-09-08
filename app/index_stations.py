fea6bd28304a23a691ac4c926d7ad2e1afe6ae0d
Chris Sinchok <chris@sinchok.com>
2013-09-08 11:56:23 -0500

    Station index work, FCC datums

--- app/index_stations.py
+++ app/index_stations.py
@@ -2,6 +2,8 @@ from pyelasticsearch import ElasticSearch
 from pyelasticsearch.exceptions import ElasticHttpNotFoundError, IndexAlreadyExistsError
 import requests
 import time
+import json
+import StringIO
 
 IGNORED_GENRES = ("9", "15", "19")  # We only care about stations that play music.
 
@@ -19,6 +21,30 @@ try:
 	es.create_index(INDEX_NAME)
 except IndexAlreadyExistsError:
 	pass
+
+STATION_MAPPING = {
+    "station" : {
+        "properties" : {
+            "band" : {"type" : "string", "index" : "not_analyzed"},
+			"call_letters": {"type" : "string", "index" : "not_analyzed"},
+			"city": {"type" : "string"},
+			"state": {"type" : "string"},
+			"country": {"type" : "string"},
+			"name": {"type" : "string"},
+			"description": {"type" : "string"},
+			"logo": {"type" : "string"},
+			"twitter": {"type" : "string"},
+			"station_site": {"type" : "string"},
+			"primary_genre": {"type" : "string", "index" : "not_analyzed"},
+			"frequency": {"type" : "string"},
+			"shoutcast_url": {"type" : "string"},
+			"location": {"type" : "geo_point"},
+			"geojson": {"type" : "string", "index" : "no"},
+        }
+    }
+}
+es.put_mapping(INDEX_NAME, "station", STATION_MAPPING)
+
 
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36'
@@ -50,6 +76,9 @@ while failures < 200:
 	if crawled_data.get("primary_genre_id") in IGNORED_GENRES:
 		continue  # We don't care about talk radio etc
 
+	if crawled_data['call_letters'].endswith("-FM"):
+		crawled_data["call_letters"] = crawled_data["call_letters"].replace("-FM", "")
+
 	index_data = {
 		"band": crawled_data.get("band"),
 		"call_letters": crawled_data.get("call_letters"),
@@ -66,24 +95,55 @@ while failures < 200:
 		"shoutcast_url": crawled_data.get("shoutcast_url"),
 	}
 
-	# TODO: get lat, lon
-	if hasattr(settings, 'GEONAMES_USER') and settings.GEONAMES_USER != "demo":
-		params = {
-			"name_equals": index_data["city"],
-			"country": index_data["country"],
-			"adminCode1": index_data["state"],
-			"maxRows": 10,
-			"lang": "en",
-			"username": settings.GEONAMES_USER,
-			"style": "medium"
+	# Now with this hack, we'll get the station coverage area from the FCC. I hope.
+	if index_data['band'].endswith("FM"):
+		fcc_data = {
+			"call": index_data["call_letters"],
+			"serv": "FM",
+			"fre2": index_data["frequency"],
+			"list": "4",
+			"NS": "N",
+			"EW": "W",
+			"size": "9"
 		}
-		geo_request = requests.get("http://api.geonames.org/searchJSON", params=params)
-		geonames = geo_request.json().get("geonames", [])
-		if geonames:
-			index_data["location"] = {
-				"lat": float(geonames[0]["lat"]),
-				"lon": float(geonames[0]["lng"])
-			}
+		r = requests.get("http://transition.fcc.gov/fcc-bin/fmq", params=fcc_data)
+		if r.status_code != 200:
+			print("Couldn't get %s: FMQ request error" % crawled_data.get("call_letters"))
+			continue
+		
+		data = r.content.strip().split("|")
+		if len(data) != 39:
+			print("Couldn't get %s: FMQ data error" % crawled_data.get("call_letters"))
+			continue
+		fcc_data.update({
+			'city': data[10].strip().replace(" ", "_"),
+			'state': data[11].strip(),
+			'dbu': 54,
+			'appid': data[-2].strip(),
+			"freq": index_data["frequency"]
+		})
+
+		KML_URL_FORMAT = "http://transition.fcc.gov/fcc-bin/contourplot.kml?appid=%(appid)s=982647&call=%(call)s&freq=%(freq)s&contour=%(dbu)s&city=%(city)s&state=%(state)s&.kml"
+		r = requests.get(KML_URL_FORMAT % fcc_data)
+		if r.status_code != 200:
+			print("Couldn't get %s: KML request error" % crawled_data.get("call_letters"))
+			continue
+
+		kml_file = StringIO.StringIO(r.content)
+		r = requests.post("http://ogre.adc4gis.com/convert", files={"upload": kml_file})
+		if r.status_code != 200:
+			print("Couldn't get %s: geojson conversion error" % crawled_data.get("call_letters"))
+			continue
+
+		geojson = r.json()
+		geojson["features"].pop()  # Remove "Alternate color"
+		coords = geojson["features"][0]["geometry"]["coordinates"]
+		index_data["location"] = {
+			"lat": float(coords[0]),
+			"lon": float(coords[1])
+		}
+		index_data["geojson"] = json.dumps(geojson)
+
 
 	es.index(INDEX_NAME, 'station', index_data, id=crawled_data.get('id'))
 
